4c01f22d3c7a8ecfe73e0d68d1f37dc1165fe1ff
Brian Rammien <brammien@students.mchenry.edu>
2015-09-04 14:15:35 -0500

    Rev 1 for week 4 homework

---
+++ projectvariablesweek4/src/main/java/com/example/Variables.java
@@ -0,0 +1,36 @@
+package com.example;
+
+public class Variables
+{
+    public static void main(String[] args) {
+        int num1 = 4;
+        boolean b = true;
+        byte bb = 55;
+        short s = 1001;
+        long l = 10L;
+        double d = 55.23;
+        char c = 'a';
+        float f = 65f;
+
+        System.out.println(num1);
+        //output for value int
+        System.out.println(bb);
+        //output for value byte
+        System.out.println(s);
+        //output for value short
+        System.out.println(l);
+        //output for value long
+        System.out.println(d);
+        //output for value double
+        System.out.println(c);
+        //output for value char
+        System.out.println(f);
+        //output for value float
+
+        if (l > s);
+            System.out.println("hello world");
+            //output value for boolean
+
+    }
+
+}
